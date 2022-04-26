ffa631f2895958dba11bef906491bc16db369eee
Vladislav Kovzov <fenix23707@gmail.com>
2022-04-26 19:03:18 +0300

    Added reactive form

--- web-programming/Lab-15/forms/src/app/comment/comment.component.ts
+++ web-programming/Lab-15/forms/src/app/comment/comment.component.ts
@@ -5,20 +5,25 @@ import {CommentService} from "../posts/services/comment.service";
 import {Comment} from "../posts/models/comment";
 import {AuthService} from "../posts/services/auth.service";
 import {User} from "../posts/models/user";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css']
 })
-export class CommentComponent implements OnInit{
+export class CommentComponent implements OnInit {
   @Input() _postId: number = -1;
+
+  commentForm: FormGroup;
 
   comments: Comment[] = [];
 
-  commentText: any
-
-  constructor(private commentService: CommentService,private authService: AuthService) {
+  constructor(private fb: FormBuilder,
+              private commentService: CommentService, private authService: AuthService) {
+    this.commentForm = fb.group({
+      text: new FormControl('', [Validators.required]),
+    })
   }
 
   ngOnInit(): void {
@@ -27,9 +32,12 @@ export class CommentComponent implements OnInit{
 
   onSubmit(): void {
     let user: User = this.authService.getUser();
-    let comment: Comment = new Comment(this.commentText, new Post("", "",this._postId), user);
+    let comment: Comment = new Comment(this.commentForm.value.text, new Post("", "", this._postId), user);
     this.commentService.save(comment)
     this.comments = this.commentService.getAllByPostId(this._postId);
-    this.commentText = "";
+  }
+
+  get text() {
+    return this.commentForm.get('text');
   }
 }
