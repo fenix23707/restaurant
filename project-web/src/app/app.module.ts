b3778a8a19ecd460297b81a204551b7ff195abfb
Vladislav Kovzov <fenix23707@gmail.com>
2022-06-05 13:01:03 +0300

    added footer

--- project-web/src/app/app.module.ts
+++ project-web/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthModule} from "./auth/auth.module";
+import {
+  FooterComponent,
+  SharedModule,
+} from './shared';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
