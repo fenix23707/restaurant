c7bc5787004cb7491abf24dc42712020a8135945
Vladislav Kovzov <fenix23707@gmail.com>
2022-06-11 10:02:30 +0300

    added login component

--- project-web/src/app/auth/login/login.component.ts
+++ project-web/src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../core";
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup
+  errors = {errors: {}};
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      'login': ['', [Validators.required, Validators.maxLength(50)]],
+      'password': ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+    });
   }
 
+  login() {
+    const data = this.loginForm.value;
+    this.errors = {errors: {}};
+
+    this.authService.login(data)
+      .subscribe(
+        user => {
+          console.log(user)
+          this.router.navigateByUrl('/')
+        },
+        error => {
+          console.log(error);
+          this.errors = error;
+        })
+  }
 }
