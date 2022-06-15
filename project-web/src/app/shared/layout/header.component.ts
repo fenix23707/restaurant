dd1556f24983a8aee51dc9669829111b125fb5e6
Vladislav Kovzov <fenix23707@gmail.com>
2022-06-15 21:40:35 +0300

    fix hamburger in header

--- project-web/src/app/shared/layout/header.component.ts
+++ project-web/src/app/shared/layout/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".menu__list");
+
+    // @ts-ignore
+    hamburger.addEventListener("click", () => {
+    // @ts-ignore
+      hamburger.classList.toggle("active");
+    // @ts-ignore
+      navMenu.classList.toggle("active");
+    });
+
+    document.querySelectorAll(".menu__list-link").forEach((n) =>
+      n.addEventListener("click", () => {
+        // @ts-ignore
+        hamburger.classList.remove("active");
+        // @ts-ignore
+        navMenu.classList.remove("active");
+      })
+    );
+
   }
 
 }
