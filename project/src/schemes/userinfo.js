6893cf089eb8594479b384f43b02ed2dd921394f
Vladislav Kovzov <fenix23707@gmail.com>
2022-06-15 16:38:57 +0300

    fix scheme

--- project/src/schemes/userinfo.js
+++ project/src/schemes/userinfo.js
@@ -4,15 +4,17 @@ const UserInfoScheme = {
     update: Joi.object().keys({
         name: Joi.string()
             .max(50)
-            .regex(/^[^0-9\s]+ [^0-9\s]+$/),
+            .regex(/^[^0-9\s]+ [^0-9\s]+$/).allow(),
         birthday: Joi.date()
             .format('YYYY-MM-DD')
-            .utc(),
+            .utc().allow(),
         phone: Joi.string()
-            .regex(/^(\+\d{1,3}\s)?\d{2}[\s.-]\d{3}([\s.-]\d{2}){2}$/),
+            .regex(/^(\+\d{1,3}\s)?\d{2}[\s.-]\d{3}([\s.-]\d{2}){2}$/).allow(),
         email: Joi.string()
             .email()
-            .max(50),
+            .max(50).allow(),
+        avatar: Joi.string()
+            .max(255).allow(),
     }),
 
     signup: {
